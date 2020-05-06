2c25a3ca099ef5babbf04f9d00f4dc544276fd82
Hixan <alex.88.elias@gmail.com>
2020-05-06 18:33:17 +1000

    added option to change default directory for saving images in org_ipython

--- org_ipython.py
+++ org_ipython.py
@@ -1,11 +1,21 @@
+import os
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pathlib import Path
+from collections import namedtuple
+
+_metavals = {'container': Path('./.images')}
+
+
+def set_default_container(container: Path):
+    _metavals['container'] = container
 
 
 def showplot_plt(name: str = None, fmt: str = 'png', container: Path = None):
+    ''' saves a matplotlib plot and prints (and returns) an org-friendly link
+    to the image. '''
     if container is None:
-        container = Path('./.images/plt/')
+        container = _metavals['container'] / Path('plt/')
 
     # create directory structure for output images
     if not container.exists():
@@ -13,7 +23,7 @@ def showplot_plt(name: str = None, fmt: str = 'png', container: Path = None):
 
     # create filename based on the time
     if name is None:
-        name = datetime.now().strftime('plot_at_%Y%m%d-%H:%M:%S.%f')
+        name = datetime.now().strftime('plot_at_%Y%m%d-%H%M%S%f')
 
     filepath = container / f'{name}.{fmt}'
 
