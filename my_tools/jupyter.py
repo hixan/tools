87bb06a46c6e01a66cd6c8e52e76ec2bb8004c7a
Hixan <alex.88.elias@gmail.com>
2020-11-11 15:32:21 +1100

    add neural network trainer

--- my_tools/jupyter.py
+++ my_tools/jupyter.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from itertools import chain
+import torch
+
 
 class LivePlot:
     """Live updates to a plot in jupyter notebook.
@@ -72,3 +74,82 @@ class LivePlot:
             self.ax.set_ylim(miny, maxy)
 
         self.fig.canvas.draw()
+
+
+def train_model(train, test, model, criterion, optimizer, nepochs=100,
+        batched=False, device=None):
+    """Train a pytorch model
+    
+    Plotting requires `%matplotlib notebook` command be run before call.
+
+    :param train: Training dataset
+        (iterable of batches if batched else iterable of data)
+    :param test: Testing/Validation dataset (for plotting purposes only)
+        same condition as with train
+    :param model: Pytorch model
+    :param criterion: Pytorch criterion object
+    :param optimizer: Pytorch optimizer object
+    :param nepochs: number of epochs to train until
+    :param batched: flag - input data is iterable of batches if True, otherwise
+        iterable of data points
+    :param device: optional: Pytorch device object to run training on.
+    """
+    if device is None:
+        # use GPU if available
+        device = torch.device("cuda:0" if torch.cuda.is_available()
+                              else "cpu")
+        
+        # move to appropriate device
+        model.to(device)
+    
+    print(f'Training model \n{model}\n on device {device} for {nepochs} epochs.')
+    
+    lossplot = LivePlot(2)
+    
+    if not batched:
+        ds_train = (train,)
+        ds_test = (test,)
+    else:
+        ds_train = train
+        ds_test = test
+
+    overall_time = 0
+    for epoch in range(nepochs + 1):
+        
+        with Timer('EpochTimer') as et:
+            # evaluate test metrics
+            loss_test = 0
+            test_n = 0
+            for X, Y in ds_test:
+                X = X.float().to(device)
+                Y = Y.long().to(device)
+                pred = model(X)
+                loss = criterion(pred, Y)
+
+                loss_test += loss.item() * X.shape[0]
+                test_n += X.shape[0]
+
+            # train and evaluate train metrics
+            loss_train = 0
+            train_n = 0
+            for X, Y in ds_train:
+                X = X.float().to(device)
+                Y = Y.long().to(device)
+                pred = model(X)
+
+                loss = criterion(pred, Y)
+
+                loss_train += loss.item() * X.shape[0]
+                train_n += X.shape[0]
+
+                if epoch != nepochs:
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+            lossplot.update((loss_train / train_n, loss_test / test_n))
+        taken = et.time_taken()
+        overall_time += taken
+        epochs_left = nepochs - epoch
+        time_left = (overall_time / (epoch + 1)) * epochs_left
+        print(f'epoch {epoch}, tr{loss_train / train_n:.2f} ts{loss_test / test_n:.2f} t{taken:.2f}s left {time_left / 60:.0f}m {time_left % 60:.0f}s', end='\r')
