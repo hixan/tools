7fe805272d6c9d86f3ec96027c52ff0d76566075
Hixan <alex.88.elias@gmail.com>
2021-03-19 13:20:55 +1100

    more elegant solution to stagger and docstring

--- my_tools/gen.py
+++ my_tools/gen.py
@@ -3,5 +3,15 @@ from itertools import islice
 
 
 def stagger(iterable, offsets=(1,)):
-    yield from zip(*(islice(iter(iterable), ofs, None) for ofs in (0, *offsets)))
+    '''stagger iterable with offsets offsets.
+    EG:
+    stagger((1, 2, 4, 8, 16), offsets=(1, 3))
+    will yield
+    (1, 2, 8),
+    (2, 4, 16)
+     ^  ^  ^- third column offset by 3
+     |  |---- second column offset by 1
+     |------- first column offset by 0
+    '''
+    return zip(*(islice(iter(iterable), ofs, None) for ofs in (0, *offsets)))
 
