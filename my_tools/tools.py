24897c5a1b49f2c255c7b2d4721022535b128726
Hixan <alex.88.elias@gmail.com>
2021-04-29 17:54:10 +1000

    re-work loudfunction to handle recursive calls

--- my_tools/tools.py
+++ my_tools/tools.py
@@ -1,6 +1,5 @@
 import inspect
-import hashlib
-import dill
+from copy import copy
 import datetime
 import pickle
 from functools import wraps, reduce
@@ -11,10 +10,10 @@ from collections import Counter, defaultdict
 import numpy as np
 from functools import reduce, partial
 import operator as op
+from itertools import chain, filterfalse
 import logging
 import os
-from typing import Callable, Mapping, Tuple, Sequence
-import base64
+from typing import Callable, Union
 
 
 def truncate(string: str, maxlength: int = 80, elipses: bool = True):
@@ -124,6 +123,206 @@ def loudfunction(outputlen: int = None, print_first: bool = True):
         return f
 
     return rvfunc
+
+
+class loudfunction:
+
+    _instances = {}  # type: ignore
+    _initialized = False  # dont run __init__ after first call
+    
+# ─━│┃┄┅┆┇┈┉┊┋┌┍┎┏┐┑┒┓└┕┖┗┘┙┚┛├┝┞┟┠┡┢┣┤┥┦┧┨┩┪┫┬┭┮┯┰┱┲┳┴┵┶┷┸┹┺┻┼┽┾┿╀╁╂╃╄╅╆╇╈╉╊╋╌
+# ╍╎╏═║╒╓╔╕╖╗╘╙╚╛╜╝╞╟╠╡╢╣╤╥╦╧╨╩╪╫╬╭╮╯╰╱╲╳╴╵╶╷╸╹╺╻╼╽╾╿
+    sections = dict(
+        single_pipe_vert = '│',
+        right_single_single_t = '├',
+        left_single_single_t = '┤',
+        right_down_double_single = '╒',
+        left_down_double_single = '╕',
+        double_pipe_horiz = '═',
+        single_pipe_horiz = '─',
+        right_up_double_single = '╘',
+        left_up_double_single = '╛',
+    )
+
+    def __new__(cls, *args, file: str = None, **kwargs):
+        if file in cls._instances:
+            return cls._instances[file]
+        rv = super(loudfunction, cls).__new__(cls)
+        cls._instances[file] = rv
+        return cls._instances[file]
+
+    def __init__(self, file: Path = None, print_first_arg: bool = True,
+                 print_formats: dict[Union[str, int], str] = None,
+                 color: bool = True, max_depth: int = 50, columns: int = None):
+        #if self._initialized:
+        #    return
+        if columns is None:
+            columns = int(os.environ['COLUMNS'])
+        self.columns = columns
+        self._initialized = True
+        self._file = file
+        self._pfa = print_first_arg
+        self.stack: list[str] = []
+        self.print_formats = print_formats
+        self.color = file is None and color
+
+        self.colors = copy(BColors.all)
+        self.colors = list(chain.from_iterable(zip(self.colors[1::2],
+                                                   self.colors[::2])))
+        self.colors *= int(max_depth // len(BColors.all) + 1)
+
+        self.color_reset = BColors.reset if self.color else ''
+
+    @property
+    def current_color(self):
+        if self.color:
+            return self.colors[len(self.stack) - 1]
+        else:
+            return ''
+
+    def __call__(self, func):
+        @wraps(func)
+        def louded_function(*args, **kwargs):
+            self.stack.append(func.__name__)
+
+            # print output
+
+            # first line
+            start0, slen0 = self._t_section_base(
+                right_key='right_down_double_single',
+                left_key='left_down_double_single')
+            start1, slen1 = self._t_section_base(
+                right_key='right_down_double_single',
+                left_key='left_down_double_single', reverse=True)
+            start_inner_len = self.columns - slen0 - slen1
+            if start_inner_len > 5:
+                start_inner_s = loudfunction._pad_str(
+                    (func.__name__, len(func.__name__)),
+                    start_inner_len, self.sections['double_pipe_horiz'])
+                print(start0 + start_inner_s + start1)
+                # arguments
+                print(self._arguments_section(args, kwargs, self.columns))
+                # seperator
+                start0, slen0 = self._t_section_base(
+                    right_key='right_single_single_t',
+                    left_key='left_single_single_t')
+                start1, slen1 = self._t_section_base(
+                    right_key='right_single_single_t',
+                    left_key='left_single_single_t', reverse=True)
+                mid_inner_len = self.columns - slen0 - slen1
+                mid_inner_s = loudfunction._pad_str(
+                    ('', 0), mid_inner_len, self.sections['single_pipe_horiz'])
+                print(start0 + mid_inner_s + start1)
+
+            # call function
+            rv = func(*args, **kwargs)
+            if start_inner_len > 5:
+                # seperator
+                print(start0 + mid_inner_s + start1)
+
+                # print the rest of the output
+                if type(rv) is tuple:
+                    print(self._arguments_section(rv, {}, self.columns))
+                else:
+                    print(self._arguments_section((rv,), {}, self.columns))
+                start0, slen0 = self._t_section_base(
+                    right_key='right_up_double_single',
+                    left_key='left_up_double_single')
+                start1, slen1 = self._t_section_base(
+                    right_key='right_up_double_single',
+                    left_key='left_up_double_single', reverse=True)
+                mid_inner_len = self.columns - slen0 - slen1
+                mid_inner_s = loudfunction._pad_str(
+                    ('', 0), mid_inner_len, self.sections['double_pipe_horiz'])
+                print(start0 + mid_inner_s + start1)
+
+            assert self.stack.pop() == func.__name__
+            return rv
+        return louded_function
+
+    def _pipe_section(self, offset=0, reverse=False, pkey='single_pipe_vert'
+                      ) -> tuple[str, int]:
+        n_chars = len(self.stack) - offset
+        lpipe = self.sections[pkey]
+        if reverse:
+            lpipe = ''.join(reversed(lpipe))
+        if self.color:
+            rv = ''
+            cls = self.colors[:n_chars]
+            if reverse:
+                cls = list(reversed(cls))
+            for col in cls:
+                rv += self.color_reset + col + lpipe
+            return rv, len(lpipe) * n_chars
+        else:
+            return lpipe * n_chars, len(lpipe) * n_chars
+
+    def _t_section_base(self, right_key = 'right_single_single_t',
+                        left_key = 'left_single_single_t', reverse=False
+                        ) -> tuple[str, int]:
+
+        ft = self.sections[right_key]
+        bt = self.sections[left_key]
+        if not reverse:
+            value, l = self._pipe_section(offset=1)
+            if self.color:
+                value += self.color_reset + self.current_color + ft
+            else:
+                value += ft
+            l += len(ft)
+        else:
+            if self.color:
+                value = self.color_reset + self.current_color + bt
+            else:
+                value = bt
+            v, l = self._pipe_section(offset=1, reverse=True)
+            value += v
+            l += len(bt)
+        return value, l
+
+    def _arguments_section(self, args: tuple, kwargs: dict, maxlen: int) -> str:
+        first_pipe_section, fps_len = self._pipe_section()
+        second_pipe_section, sps_len = self._pipe_section(reverse=True)
+
+        outs = []
+        inner_len = maxlen - fps_len - sps_len
+        strs = loudfunction._allarg_string(
+            args, kwargs, inner_len, self.current_color,
+            self.color_reset)
+        for s in strs:
+            outs.append(first_pipe_section
+                        + loudfunction._pad_str(s, inner_len, ' ')
+                        + second_pipe_section)
+        return '\n'.join(outs)
+
+    @staticmethod
+    def _pad_str(string: tuple[str, int], to_length: int, pad_value: str) -> str:
+        return string[0] + pad_value * (to_length - string[1])
+
+    @staticmethod
+    def _allarg_string(args, kwargs, maxlen, curr_color, reset_color, /):
+        sts = []
+        pad = ' ' * maxlen
+        for arg in args:
+            sts.append(loudfunction._default_arg_string(arg, maxlen, curr_color, reset_color))
+        for k, v in kwargs.items():
+            sts.append((reset_color + k[:maxlen-1] + ':', min(len(k)+1, maxlen)))
+            sts.append(loudfunction._default_arg_string(v, maxlen, curr_color, reset_color))
+        return sts
+
+    @staticmethod
+    def _default_arg_string(arg, maxlen, curr_color, reset_color, /) -> tuple[str, int]:
+        tpe = str(type(arg)).replace('\n', ' ')
+        st = repr(arg).replace('\n', ' ')
+        la = len(tpe) + len(st) + 3
+        if len(tpe) < maxlen / 5 or la < maxlen:
+            return f'{tpe} {curr_color}-{reset_color} {st}', la
+        else:
+            return st[:maxlen], min(len(st), maxlen)
+
+
+def strlen_printable(string: str) -> int:
+    return len(list(filterfalse(op.methodcaller('isprintable'), string)))
 
 
 def loudmethod(outputlen=None):
@@ -332,15 +531,33 @@ class DivCounter(Counter):
 
 
 class BColors:
-    c1 = '\033[95m'
-    c2 = '\033[94m'
-    c3 = '\033[96m'
-    c4 = '\033[92m'
-    c5 = '\033[93m'
-    c6 = '\033[91m'
-    c7 = '\033[0m'
-    c8 = '\033[1m'
-    c9 = '\033[4m'
+    black    = '\u001b[30m'
+    red      = '\u001b[31m'
+    green    = '\u001b[32m'
+    yellow   = '\u001b[33m'
+    blue     = '\u001b[34m'
+    magenta  = '\u001b[35m'
+    cyan     = '\u001b[36m'
+    white    = '\u001b[37m'
+    bblack   = '\u001b[30;1m'
+    bred     = '\u001b[31;1m'
+    bgreen   = '\u001b[32;1m'
+    byellow  = '\u001b[33;1m'
+    bblue    = '\u001b[34;1m'
+    bmagenta = '\u001b[35;1m'
+    bcyan    = '\u001b[36;1m'
+    bwhite   = '\u001b[37;1m'
+
+    all: list[str] = []
+
+    reset    = '\u001b[0m'
+
+BColors.all = [
+        BColors.black, BColors.red, BColors.green, BColors.yellow,
+        BColors.blue, BColors.magenta, BColors.cyan, BColors.white,
+        BColors.bblack, BColors.bred, BColors.bgreen, BColors.byellow,
+        BColors.bblue, BColors.bmagenta, BColors.bcyan, BColors.bwhite,
+    ]
 
 
 class Timer:
@@ -393,41 +610,3 @@ class Timer:
         completed)'''
         assert not self.started, f'Timer {repr(self.name)} still running!'
         return self.laps[-1]
-
-
-"""
-def cachify(function):
-    '''Wrap function to save its returned value to a file if not previously run,
-    and load the file instead of re-computing if it has been.
-
-    The wrapped function needs an additional argument: filename.
-    An optional extra argument 'overwrite' allows the function to always
-    recalculate and save output, overwriting previously cached calls.
-    '''
-    root = Path('./.cache')
-    @wraps(function)  # keep original docstring and name
-    def rv(*args, filename=None, overwrite=False, **kwargs):
-        # sanitize filename (construct default)
-        if filename is None:
-            raise TypeError(f'{function.__name__} missing 1 required keyword '
-                    'argument: \'filename\'\n'
-                    'This argument is required as the function has been '
-                    'cachified.')
-        
-        file = root / filename
-        if file.exists() and not overwrite:
-            # load saved value
-            with file.open('rb') as f:
-                return pickle.load(f)
-        
-        fret = function(*args, **kwargs)
-
-        # save the value
-        with file.open('wb') as f:
-            pickle.dump(fret, f)
-
-        return fret
-
-    return rv
-"""
-
